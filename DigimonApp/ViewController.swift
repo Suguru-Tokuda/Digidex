dfb84d36ee93f20d485a6a6963f3be247cbb395b
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-02 14:10:12 -0400

    Initial Commit

---
+++ DigimonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigimonApp
+//
+//  Created by Suguru Tokuda on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
